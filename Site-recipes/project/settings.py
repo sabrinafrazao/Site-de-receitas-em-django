101cc9e6494c0dfe8fdaf7c3581e939255f9324c
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2024-01-17 17:37:51 -0400

    apagando arquivos inuteis

--- Site-recipes/project/settings.py
+++ Site-recipes/project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
